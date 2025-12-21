2c1e4d652aed75519f51bcf125112b5363f4e2f0
teacher-vagner <teacher.vagnerrocha79@gmail.com>
2025-12-21 15:39:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-{const CONFIG = {
-  escola: "EMEF PROFESSOR JOÃO DA SILVA",
-  emailProfessor: "teacher.vagnerrocha79@gmail.com",
-  ano: "6º Ano",
-  bimestre: "1º Bimestre",
-  bncc: "EF06LI01, EF06LI02"
+const CONFIG = {
+  escola: "ESCOLA TESTE FUNCIONANDO",
+  emailProfessor: "teacher.vagnerrocha79@gmail.com"
 };
