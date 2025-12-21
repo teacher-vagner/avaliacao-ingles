74db4eb4b552b2eeb6977993878640ec94bbbf4b
teacher-vagner <teacher.vagnerrocha79@gmail.com>
2025-12-21 14:26:58 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+{const CONFIG = {
+  escola: "EMEF PROFESSOR JOÃO DA SILVA",
+  emailProfessor: "teacher.vagnerrocha79@gmail.com",
+  ano: "6º Ano",
+  bimestre: "1º Bimestre",
+  bncc: "EF06LI01, EF06LI02"
+};
