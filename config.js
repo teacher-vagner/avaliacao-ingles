94ba9761f5dd5db9f32f71e5bca4869b13051563
teacher-vagner <teacher.vagnerrocha79@gmail.com>
2025-12-21 20:36:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,29 +1,5 @@
 const CONFIG = {
-  escola: "ESCOLA TESTE FUNCIONANDO",
+  escola: "English Vagner's School",
   emailProfessor: "teacher.vagnerrocha79@gmail.com"
 };
 
-let html = `<h3>${i+1}. (${q.habilidade})</h3><p>${q.q}</p>`;
-
-if(q.imagem){
-  html += `<img src="${q.imagem}" style="max-width:100%;margin:10px 0">`;
-}
-
-if(q.audio){
-  html += `
-    <audio controls style="margin:10px 0;width:100%">
-      <source src="${q.audio}" type="audio/mpeg">
-      Seu navegador não suporta áudio.
-    </audio>
-  `;
-}
-
-
-if(q.video){
-  html += `
-    <video controls style="max-width:100%;margin:10px 0">
-      <source src="${q.video}" type="video/mp4">
-      Seu navegador não suporta vídeo.
-    </video>
-  `;
-}
